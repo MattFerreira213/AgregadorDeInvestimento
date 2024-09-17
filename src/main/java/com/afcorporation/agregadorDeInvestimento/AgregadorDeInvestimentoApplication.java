3979b896a7df29955a40be205c16f10ec87b45a8
MattFerreira213 <mateusanjos225@gmail.com>
2024-09-17 20:33:23 -0300

    first commit

---
+++ src/main/java/com/afcorporation/agregadorDeInvestimento/AgregadorDeInvestimentoApplication.java
@@ -0,0 +1,13 @@
+package com.afcorporation.agregadorDeInvestimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class git initApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadorDeInvestimentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afcorporation/agregadorDeInvestimento/AgregadorDeInvestimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afcorporation.agregadorDeInvestimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorDeInvestimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
