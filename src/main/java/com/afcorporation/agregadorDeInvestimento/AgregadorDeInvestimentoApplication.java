2173560267fa003c9816108d7470a66ac3d249d6
MattFerreira213 <mateusanjos225@gmail.com>
2024-09-17 20:34:04 -0300

    correção rapida

--- src/main/java/com/afcorporation/agregadorDeInvestimento/AgregadorDeInvestimentoApplication.java
+++ src/main/java/com/afcorporation/agregadorDeInvestimento/AgregadorDeInvestimentoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class git initApplication {
+public class AgregadorDeInvestimentoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgregadorDeInvestimentoApplication.class, args);
